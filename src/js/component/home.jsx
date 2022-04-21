827e74d9b11f8f670fb82d5b234f1a3971160733
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-21 13:27:26 +0000

    avance plantilla

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import "animate.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [symbol, setSymbol] = useState("X");
+	// Lista de componentes necesarios:
+	// Componente de tablero
+	// Componente de inicio del juego
+	// Componente de reseteo
+	// Posibles opciones de juego
+	// Función que determine al ganador
+	// Función de jugadores
+
 	return (
-		<div>
-			
-		</div>
+		<>
+			<h1 id="tittle" className="animate__jackInTheBox">
+				Tic Tac Toe in React.js
+			</h1>
+			<div className="container ">
+				<table className="table table-borderless mt-5 ">
+					<tbody className="g-2">
+						<tr>
+							<td key={1} className="borderR" id="borderB">
+								{symbol}
+							</td>
+							<td key={2} className="borderR" id="borderB">
+								{symbol}
+							</td>
+							<td key={3} id="borderB">
+								{symbol}
+							</td>
+						</tr>
+						<tr>
+							<td key={4} className="borderR" id="borderB">
+								{symbol}{" "}
+							</td>
+							<td key={5} className="borderR" id="borderB">
+								{symbol}
+							</td>
+							<td key={6} id="borderB">
+								{symbol}
+							</td>
+						</tr>
+						<tr>
+							<td key={7} className="borderR ">
+								{symbol}
+							</td>
+							<td key={8} className="borderR ">
+								{symbol}
+							</td>
+							<td key={9}>{symbol}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</>
 	);
 };
 
